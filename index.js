d3d3ca565d153f96cd8abcf893ea88c8d25c8eb5
ray-nis <reirei256@gmail.com>
2019-01-23 09:14:11 +0100

    js

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 $(document).ready(function () {
     $('#header').load('pages/header.html');  
 });
-
-
-function menuSlide() {
-    $(".nav").slideToggle("fast", "linear");
-}
-$("#menuButton").click(menuSlide);
