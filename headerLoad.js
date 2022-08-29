a1fac90fa7647d22c3fa6f02ef9afacc9f9a84f3
ray-nis <42224913+ray-nis@users.noreply.github.com>
2022-08-29 20:11:00 +0200

    Update headerLoad.js

--- headerLoad.js
+++ headerLoad.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-    $('#header').load("/includes/header.html");  
+    $('#header').load("./includes/header.html");  
 });
 
 $(document).ready(function () {
-    $('#footer').load("/includes/footer.html");
+    $('#footer').load("./includes/footer.html");
 });
 
